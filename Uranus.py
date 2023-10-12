a56dc4ea7b60dba8b7feef45d9f5104831173cc0
yuriorl <147401174+yuriorl@users.noreply.github.com>
2023-10-12 16:32:04 +0700

    Add files via upload

---
+++ Uranus.py
@@ -0,0 +1,3 @@
+print("Hello!")
+print("The squared number you input: " + str(int(input())**2))
+print("Googbye")
---
+++ bashscript.sh
@@ -0,0 +1,5 @@
+read a
+echo "Hello"
+let "a=a**2"
+echo "The squared number you input: $a"
+echo "Goodbye but on bash"
